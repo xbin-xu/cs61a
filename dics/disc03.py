ee177ac54e7c2c494b48a52da73e1b82ded50271
Bin <1073788479@qq.com>
2024-04-21 08:40:41 +0800

    upload disc03

---
+++ dics/disc03.py
@@ -0,0 +1,116 @@
+# Q1: Swipe
+def swipe(n):
+    """Print the digits of n, one per line, first backward then forward.
+
+    >>> swipe(2837)
+    7
+    3
+    8
+    2
+    8
+    3
+    7
+    """
+    if n < 10:
+        print(n)
+    else:
+        "*** YOUR CODE HERE ***"
+
+
+# Q2: Skip Factorial
+def skip_factorial(n):
+    """Return the product of positive integers n * (n - 2) * (n - 4) * ...
+
+    >>> skip_factorial(5) # 5 * 3 * 1
+    15
+    >>> skip_factorial(8) # 8 * 6 * 4 * 2
+    384
+    """
+    if _____:
+        return _____
+    else:
+        return n * skip_factorial(n - 2)
+
+
+# Q3: Is Prime
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+
+# Q4: Recursive Hailstone
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the number of elements in the sequence.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    print(n)
+    if n % 2 == 0:
+        return even(n)
+    else:
+        return odd(n)
+
+
+def even(n):
+    return _____
+
+
+def odd(n):
+    "*** YOUR CODE HERE ***"
+# Q5: Sevens
+def sevens(n, k):
+    """Return the (clockwise) position of who says n among k players.
+
+    >>> sevens(2, 5)
+    2
+    >>> sevens(6, 5)
+    1
+    >>> sevens(7, 5)
+    2
+    >>> sevens(8, 5)
+    1
+    >>> sevens(9, 5)
+    5
+    >>> sevens(18, 5)
+    2
+    """
+
+    def f(i, who, direction):
+        if i == n:
+            return who
+        "*** YOUR CODE HERE ***"
+    return f(1, 1, 1)
+
+
+def has_seven(n):
+    if n == 0:
+        return False
+    elif n % 10 == 7:
+        return True
+    else:
+        return has_seven(n // 10)
+
+
+# Q6: Karel the Robot
+# https://compedu.stanford.edu/karel-reader/docs/python/en/intro.html
