e1f513766abe1f0031d5825d59bd06cc4fef924e
Bin <1073788479@qq.com>
2024-04-23 22:40:23 +0800

    finish disc04: Tree Recursion

---
+++ dics/disc04.py
@@ -0,0 +1,75 @@
+# Q1: Insect Combinatorics
+def paths(m, n):
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    # move (1, 1) to (1, ?) or (?, 1) has only one path
+    if m == 1 and n == 1:
+        return 1
+    return paths(m - 1, n) + paths(m, n - 1)
+
+    # from (m, n) to (1, 1), every times has two choies move down or left
+    # if m < 1 or n < 1:
+    #     return 0
+    # if m == 1 and n == 1:
+    #     return 1
+    # return paths(m - 1, n) + paths(m, n - 1)
+
+
+# Q2: Max Product
+def max_product(s):
+    """Return the maximum product of non-consecutive elements of s.
+
+    >>> max_product([10, 3, 1, 9, 2])   # 10 * 9
+    90
+    >>> max_product([5, 10, 5, 10, 5])  # 5 * 5 * 5
+    125
+    >>> max_product([])                 # The product of no numbers is 1
+    1
+    """
+    if len(s) == 0:
+        return 1
+    elif len(s) == 1:
+        return s[0]
+    else:
+        return max(s[0] * max_product(s[2:]), max_product(s[1:]))
+        # return max(s[0] * max_product(s[2:]), s[1] * max_product(s[3:]))
+
+
+# Q3: Sum Fun
+def sums(n, m):
+    """Return lists that sum to n containing positive numbers up to m that
+    have no adjacent repeats.
+
+    >>> sums(5, 1)
+    []
+    >>> sums(5, 2)
+    [[2, 1, 2]]
+    >>> sums(5, 3)
+    [[1, 3, 1], [2, 1, 2], [2, 3], [3, 2]]
+    >>> sums(5, 5)
+    [[1, 3, 1], [1, 4], [2, 1, 2], [2, 3], [3, 2], [4, 1], [5]]
+    >>> sums(6, 3)
+    [[1, 2, 1, 2], [1, 2, 3], [1, 3, 2], [2, 1, 2, 1], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    """
+    if n < 0:
+        return []
+    if n == 0:
+        sums_to_zero = []  # The only way to sum to zero using positives
+        return [sums_to_zero]  # Return a list of all the ways to sum to zero
+    result = []
+    for k in range(1, m + 1):
+        result = result + [
+            [k] + rest for rest in sums(n - k, m) if rest == [] or k != rest[0]
+        ]
+    return result
