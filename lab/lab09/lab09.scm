d13591b4474aa113bd94222b7ff8583a12d89b95
Bin <xbin.xu@qq.com>
2025-01-28 16:20:45 +0800

    feat: finish lab09 Scheme

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,10 +1,34 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+; if
+; (define (over-or-under num1 num2)
+;   (if (= num1 num2)
+;       0
+;       (if (< num1 num2)
+;           -1
+;           1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+; cond
+(define (over-or-under num1 num2)
+  (cond 
+    ((= num1 num2) 0)
+    ((< num1 num2) -1)
+    (else 1)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (make-adder num)
+  (lambda (inc) (+ inc num)))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (composed f g)
+  (lambda (x) (f (g x))))
+
+; (define (repeat f n)
+;   (lambda (x)
+;     (if (< n 1)
+;         x
+;         (f ((repeat f (- n 1)) x)))))
+
+(define (repeat f n)
+  (if (< n 1)
+      (lambda (x) x)
+      (composed f (repeat f (- n 1)))))
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +40,12 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+; (define (gcd a b)
+;   (if (zero? b)
+;       a
+;       (gcd (min a b) (modulo (max a b) (min a b)))))
+
+(define (gcd a b)
+  (if (zero? b)
+      a
+      (gcd b (modulo a b))))
