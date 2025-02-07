ddcca835a6b10121daefeabd96334b85a8b952e8
Bin <xbin.xu@qq.com>
2025-02-07 17:57:28 +0800

    feat: finish hw08 Scheme Lists

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,7 +1,21 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+    (cond
+        ((or (null? s) (null? (cdr s))) #t)
+        (else (and (<= (car s) (car (cdr s))) (ascending? (cdr s))))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+    (cond
+        ((null? s) nil)
+        ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+    (cond
+        ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else (cons (car lst1) (interleave lst2 (cdr lst1))))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (no-repeats s)
+    (cond
+        ((null? s) nil)
+        (else (cons (car s) (no-repeats (filter (lambda (x) (not (= x (car s)))) (cdr s)))))))
